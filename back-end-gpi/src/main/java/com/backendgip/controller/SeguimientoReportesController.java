111bbf595c8fc85f89dd1648d1d6b044c3d43945
Sleider Jair Sevillano Ramos <ssevillano@itssolutions.co>
2024-03-18 08:55:15 -0500

    Cambios reporte seguimiento

--- back-end-gpi/src/main/java/com/backendgip/controller/SeguimientoReportesController.java
+++ back-end-gpi/src/main/java/com/backendgip/controller/SeguimientoReportesController.java
@@ -3,20 +3,27 @@ package com.backendgip.controller;
 import com.backendgip.model.TipoReporte;
 
 import com.backendgip.repository.TipoReporteRepository;
+import com.backendgip.service.ProyectoService;
 import com.backendgip.service.TipoReporteService;
+
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.backendgip.model.RecursoActividad;
+import com.backendgip.service.RecursoActividadService;
 
 @RestController
 @Transactional
 @RequestMapping({ "/api/reportes" })
 public class SeguimientoReportesController {
     @Autowired
-	private TipoReporteService tipoReporteService;
+    private RecursoActividadService recursoActividadService;
+    
 	@Autowired
 	private TipoReporteRepository tipoRepository;
 
@@ -28,6 +35,21 @@ public class SeguimientoReportesController {
 		return (List) this.tipoRepository.findAll();
 	}
 
-	
+	@GetMapping("/reporte")
+	public List<RecursoActividad> getAllProyectos() {
+		List<RecursoActividad> reportesentrada = this.recursoActividadService.getRecursoActividad();
+		List<RecursoActividad> reportesInactivos = new ArrayList<>();
+		
+		for (RecursoActividad reporte : reportesentrada) {
+			// Verificar si el proyecto está inactivo (estado "CERRADO")
+			if ("CERRADO".equals(reporte.getActividad().getProyecto().getEstadoProyecto().getEstado())) {
+				reportesInactivos.add(reporte); // Agregar el proyecto a la lista de inactivos
+			}
+		}
+		
+		return reportesInactivos; // Devolver solo los proyectos inactivos
+	}
+	 
 
+	
 }
